5a8ac76dc53ac0ff5690cdc05ab245ba92f18a37
riyad-96 <riyadofficial.ide@gmail.com>
2025-12-09 00:30:57 +0600

    working on admin dashboard

--- src/pages/admin/dashboard/AdminDashboard.tsx
+++ src/pages/admin/dashboard/AdminDashboard.tsx
@@ -1,6 +1,6 @@
 export default function AdminDashboard() {
   return (
-    <div className="bg-green-400 px-2 md:px-3">
+    <div className="px-2 md:px-3">
       <div>AdminDashboard</div>
     </div>
   );
